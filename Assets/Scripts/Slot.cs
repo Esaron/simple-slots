e789e2cae66e1700b1f7614d01769af84ccb8503
Esaron <spam.esaron@gmail.com>
2025-03-11 10:08:30 -0400

    Fix some math

--- Assets/Scripts/Slot.cs
+++ Assets/Scripts/Slot.cs
@@ -6,17 +6,20 @@ public class Slot : MonoBehaviour
 {
     private const float FullRotation = 360f;
     private int _currentState = 0;
+    private int _previousState = 0;
     
     public async Task<int> Spin()
     {
+        _previousState = _currentState;
         _currentState = UnityEngine.Random.Range(0, 8);
-        float rotateDegrees = (UnityEngine.Random.Range(3, 7) + _currentState / 8f) * FullRotation;
+        float rotateDegrees = (UnityEngine.Random.Range(2, 5) + (_previousState - _currentState) / 8f) * FullRotation;
         float end = Time.time + rotateDegrees/FullRotation;
         while (Time.time < end)
         {
-            transform.Rotate(Vector3.right, FullRotation * Time.deltaTime);
+            transform.Rotate(Vector3.left, FullRotation * Time.deltaTime, Space.Self);
             await Task.Yield();
         }
+        transform.rotation = Quaternion.Euler(Vector3.left * (_currentState / 8f * FullRotation));
         return _currentState;
     }
 }
